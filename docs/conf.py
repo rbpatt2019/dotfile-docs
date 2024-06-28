5412bc1cd2d7548094c36dfb8ab3bcf98299729d
rbpatt2019 <rb.patterson.cross@gmail.com>
2024-06-28 13:59:38 +0100

    docs(conf.py): add git buttons

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,16 @@ extensions = [
 pygments_style = "sphinx"
 html_theme = "sphinx_book_theme"
 
+html_theme_options = {
+    "repository_url": "https://github.com/rbpatt2019/dotfile-docs",
+    "repository_branch": "main",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_source_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+}
+
 source_suffix = {
     ".rst": "restructuredtext",
     ".md": "markdown",
