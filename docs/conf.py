c3c20012f0b8805d0e0d7e25e3a93ecdff7890f8
rbpatt2019 <rb.patterson.cross@gmail.com>
2024-07-22 15:29:46 +0100

    feat(conf.py): configure logo

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,9 @@ extensions = [
     "sphinx_tabs.tabs",
 ]
 pygments_style = "sphinx"
-html_theme = "sphinx_book_theme"
 
+html_logo = "_static/dragon-solid.svg"
+html_theme = "sphinx_book_theme"
 html_theme_options = {
     "repository_url": "https://github.com/rbpatt2019/dotfile-docs",
     "repository_branch": "main",
